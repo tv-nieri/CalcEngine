490d8c86a018011a982e15a1d64c5d22c69f9b44
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-06-03 13:28:01 -0300

    String formating.

--- src/br/com/tvn/calcengine/Main.java
+++ src/br/com/tvn/calcengine/Main.java
@@ -4,20 +4,81 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double[] leftVals = {100.0d, 25.0d, 255.0d, 11.0d};
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
         double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d','a', 's', 'm'};
         double[] results = new double[opCodes.length];
 
-
-        for (int i = 0; i < opCodes.length; i++) {
-            results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-        }
-
-        for (double currentVal : results) {
-            System.out.println(currentVal);
+        if (args.length == 0) {
+            for (int i = 0; i < opCodes.length; i++) {
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+            for (double currentResult : results) {
+                System.out.println(currentResult);
+            }
+        } else if (args.length == 1 && args[0].equals("interactive")){
+            executeInteractively();
+        } else if (args.length == 3) {
+            handleCommandLine(args);
+        } else {
+            System.out.println("Please provide an operations and two numeric values");
         }
     }
+
+
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displyResult(opCode, leftVal, rightVal, result);
+    }
+
+
+    private static void displyResult(char opCode, double leftVal, double rightVal, double result) {
+        char symbol = symbolFromOpCode(opCode);
+        String builder = leftVal +
+                " " +
+                symbol +
+                " " +
+                rightVal +
+                " = " +
+                result;
+        System.out.println(builder);
+    }
+
+
+    private static char symbolFromOpCode(char opCode) {
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+        for (int index = 0; index < opCodes.length; index++) {
+            if (opCode == opCodes[index]) {
+                symbol = symbols[index];
+                break;
+            }
+        }
+        return symbol;
+    }
+
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+    }
+
 
     static double execute(char opCode, double leftVal, double rightVal){
         double result;
@@ -40,5 +101,26 @@ public class Main {
                 break;
         }
         return result;
+    }
+
+
+    static char opCodeFromString(String operationName) {
+        return operationName.charAt(0);
+    }
+
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        double value = 0.d;
+        for (int index = 0; index < numberWords.length; index++) {
+            if (word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 }
