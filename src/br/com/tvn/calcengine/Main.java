50671d84491ed2aa6862e3fc64b1b33f9d6778d4
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-06-03 11:54:14 -0300

    implement execute()

--- src/br/com/tvn/calcengine/Main.java
+++ src/br/com/tvn/calcengine/Main.java
@@ -4,39 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double[] leftVals = {100.0, 25.0, 255.0, 11.0};
-        double[] rightVals = {50.0, 92.0, 17.0, 3.0};
+        double[] leftVals = {100.0d, 25.0d, 255.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d','a', 's', 'm'};
         double[] results = new double[opCodes.length];
-        double value1 = 100.0d;
-        double value2 = 50.0d;
-        double result = 0.0d;
-        char opCode = 'd';
 
 
         for (int i = 0; i < opCodes.length; i++) {
-            switch (opCodes[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d; //ignored
-                    break;
-                default:
-                    System.out.println("Invalid opCode: " + opCode);
-                    results[i] = 0.0d;
-                    break;
-            }
+            results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
         }
 
         for (double currentVal : results) {
             System.out.println(currentVal);
         }
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal){
+        double result;
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+        return result;
     }
 }
